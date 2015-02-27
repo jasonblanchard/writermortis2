02485193240aad9aa64130fda195e8db2b20f3a7
Jason Blanchard <jas.blanchard@gmail.com>
2015-02-26 19:14:40 -0500

    Updated subprojects

--- start-servers.sh
+++ start-servers.sh
@@ -10,7 +10,7 @@ tmux send-keys -t writermortis:2 "cd writermortis_realtime && nf start" C-m
 
 tmux new-window -t writermortis:1 -n consle
 tmux select-window -t writermortis:1
-tmux send-keys -t writermortis:1 "cd writermortis2 && rails s" C-m
+tmux send-keys -t writermortis:1 "cd writermortis_server && rails s" C-m
 tmux split-window
 tmux send-keys -t writermortis:1 "cd writermortis_client && ember serve" C-m
 

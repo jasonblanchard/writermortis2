6ab91a644bc8f18d4f8cf6dd98a59c755b02152b
Jason Blanchard <jas.blanchard@gmail.com>
2015-03-28 08:45:25 -0400

    UPdated client projects and tmux script

--- start-servers.sh
+++ start-servers.sh
@@ -8,14 +8,14 @@ tmux select-window -t writermortis:0
 tmux rename-window 'realtime'
 tmux send-keys -t writermortis:0 "cd writermortis_realtime && nf start" C-m
 
-tmux new-window -t writermortis:3 -n testing
-tmux select-window -t writermortis:3
-tmux send-keys -t writermortis:3 "cd writermortis_client && ember test --server" C-m
-
 tmux new-window -t writermortis:1 -n console
 tmux select-window -t writermortis:1
+tmux split-window -h
+tmux select-pane -t 0
 tmux send-keys -t writermortis:1 "cd writermortis_server && rails s" C-m
 tmux split-window
 tmux send-keys -t writermortis:1 "cd writermortis_client && ember serve --proxy http://localhost:3000" C-m
+tmux select-pane -t 2
+tmux send-keys -t writermortis:1 "cd writermortis_client && ember test --server" C-m
 
 tmux attach -t writermortis
